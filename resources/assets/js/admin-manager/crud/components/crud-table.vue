26254640511a506889365f4979a73fc31ca3c9ac
vshapovalov <shapovalov.v@gmail.com>
2018-01-31 14:26:27 +0600

    ordering, filtering in table

--- resources/assets/js/admin-manager/crud/components/crud-table.vue
+++ resources/assets/js/admin-manager/crud/components/crud-table.vue
@@ -2,7 +2,9 @@
     <table class="table crud-table">
         <thead>
         <tr>
-            <th data-role="button" v-for="field in crudBrowsableFields" @click.stop="sort(field)">{{ field.caption }}</th>
+            <th data-role="button" v-for="field in crudBrowsableFields"
+                @click.stop="sort(field)"><i
+                    class="fa" :class="{'fa-arrow-up': sorting[field.name] ==='asc', 'fa-arrow-down': sorting[field.name] ==='desc'}"></i>{{ field.caption }}</th>
             <!--<th> Действия</th>-->
             <th v-if="rowActions.indexOf('edit') >= 0" style="width: 30px; text-align:center;"><i class="fa fa-pencil"></i></th>
             <th v-if="rowActions.indexOf('delete') >= 0" style="width: 30px; text-align:center;"><i class="fa fa-trash"></i></th>
@@ -12,7 +14,16 @@
         <tr v-show="items && items.length === 0">
             <td>Нет записей</td>
         </tr>
-        <tr v-for="item in items" @click.prevent="selectItem(item)" :class="{'is-selected': item.isSelected}">
+
+        <tr v-show="items && items.length">
+            <td v-for="field in crudBrowsableFields">
+                <input v-if="isFilterVisible(field)" type="text" class="input" :field-name="field.name"
+                       @keyup.stop.prevent="filter" placeholder="Поиск">
+            </td>
+        </tr>
+
+
+        <tr v-for="item in preparedItems" @click.prevent="selectItem(item)" :class="{'is-selected': item.isSelected}">
             <td v-for="field in crudBrowsableFields">
                 <crud-caption :item="item" :field="field" :is-pivot="field.isPivot"></crud-caption>
             </td>
@@ -52,10 +63,46 @@
         },
         data: function () {
             return {
+                sorting: {},
+                filtering: {},
 
+                filterHelper: {
+                    fields: {},
+                    debouncedFilter: null
+                }
             }
         },
         computed: {
+            preparedItems(){
+
+                let items = [];
+
+                let filterFields = _.keys(this.filtering);
+
+                if (filterFields.length) {
+
+                    items = _.filter(this.items, (i)=>{
+
+                        return _.every(filterFields, (ff)=>{
+
+                            return i[ff] && (_.lowerCase(i[ff]).indexOf(this.filtering[ff]) >=0);
+                        });
+                    });
+
+                } else {
+                    items = this.items;
+                }
+
+                let sortFields = _.keys(this.sorting);
+
+                if (sortFields.length){
+                    let sortDirection = _.values(this.sorting);
+
+                    items = _.orderBy(items, sortFields, sortDirection );
+                }
+
+                return items;
+            },
             crudBrowsableFields(){
 
                 let fields = _.filter(
@@ -77,6 +124,30 @@
             relationTypes(){ return RelationTypes; }
         },
         methods: {
+            isFilterVisible(field){
+                // TODO: we need field display value resolver, for using filter fo all field types
+
+                return (field.type === FieldTypes.TEXTAREA) || (field.type === FieldTypes.TEXTBOX)
+                    || (field.type === FieldTypes.COLORBOX) || (field.type === FieldTypes.DATEPICKER);
+            },
+
+            filter(e){
+                if (e.target.value.length) {
+                    this.filterHelper.fields[e.target.getAttribute('field-name')] = e.target.value;
+                } else {
+                    delete this.filterHelper.fields[e.target.getAttribute('field-name')];
+                }
+
+                this.filterHelper.debouncedFilter();
+            },
+
+            applyFilter(){
+
+
+                this.filtering = _.clone(this.filterHelper.fields);
+
+            },
+
             selectItem(item){
 
                 if (this.isPickMode){
@@ -108,8 +179,20 @@
 
             },
             sort(field){
-                this.$emit('sort', field.name);
+
+                if (!_.has(this.sorting, field.name)) {
+                    this.$set(this.sorting, field.name, 'asc');
+                } else {
+                    if (this.sorting[field.name] === 'desc') {
+                        this.$delete(this.sorting , field.name);
+                    } else {
+                        this.sorting[field.name] = 'desc';
+                    }
+                }
             }
+        },
+        beforeMount(){
+            this.filterHelper.debouncedFilter = _.debounce( this.applyFilter, 1000);
         },
         mounted() {
 
